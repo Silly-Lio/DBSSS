e74df8e59f42310ee6646d82dac37f77de9fde59
ZherenLiu <65768893+Silly-Lio@users.noreply.github.com>
2022-10-23 21:25:05 +0800

    Update SQL_SimilarSeqSearch_StrictSimilar.sql

--- SQL_SimilarSeqSearch_StrictSimilar.sql
+++ SQL_SimilarSeqSearch_StrictSimilar.sql
@@ -1,37 +1,34 @@
 WITH target_taz AS ( -- 0. 表格生成与存储
-					SELECT *
-					FROM trajs trajs
-					WHERE trajs.tazid = {target_tazid}
-			), 
-			cp_dist AS (
-					SELECT matchtrajs.tazid
-					FROM (  SELECT * 
-									FROM cp_trajs
-									WHERE tazid = {target_tazid}) target, 
-					LATERAL (SELECT *
-									 FROM cp_trajs cp_trajs
-									 ORDER BY target.geom <-> cp_trajs.geom ASC 
-									 LIMIT {k*5}) as matchtrajs
-			), table_cp_dist AS (
-					SELECT trajs.*
-					FROM cp_dist 
-					INNER JOIN trajs trajs 
-					ON cp_dist.tazid = trajs.tazid -- ORDER BY st_centroid(target.geom) <-> st_centroid(trajs.geom) ASC 
-			),
-
-			table_area AS ( -- 2. 多边形面积
-					SELECT trajs.tazid, trajs.geom
-					FROM table_cp_dist trajs, target_taz target
-					ORDER BY ABS(st_area(st_envelope(target.geom)) - st_area(st_envelope(trajs.geom))) ASC 
-					LIMIT {k*2}
-			), 
-
-			table_f_dist AS ( -- 3. Frechet 距离
-					SELECT trajs.tazid, trajs.geom
-					FROM table_area trajs, target_taz target
-					ORDER BY st_frechetdistance(target.geom, trajs.geom) ASC 
-					LIMIT {k}
-			)
+	SELECT *
+	FROM trajs trajs
+	WHERE trajs.tazid = {target_tazid}
+     ), 
+     cp_dist AS (
+	SELECT matchtrajs.tazid
+	FROM (  SELECT * 
+		FROM cp_trajs
+		WHERE tazid = {target_tazid}) target, 
+	LATERAL (SELECT *
+		 FROM cp_trajs cp_trajs
+		 ORDER BY target.geom <-> cp_trajs.geom ASC 
+		 LIMIT {k*5}) as matchtrajs
+     ), table_cp_dist AS (
+	SELECT trajs.*
+	FROM cp_dist 
+	INNER JOIN trajs trajs 
+	ON cp_dist.tazid = trajs.tazid -- ORDER BY st_centroid(target.geom) <-> st_centroid(trajs.geom) ASC 
+     ), table_area AS ( -- 2. 多边形面积
+	SELECT trajs.tazid, trajs.geom
+	FROM table_cp_dist trajs, target_taz target
+	ORDER BY ABS(st_area(st_envelope(target.geom)) - st_area(st_envelope(trajs.geom))) ASC 
+	LIMIT {k*2}
+     ), 
+	table_f_dist AS ( -- 3. Frechet 距离
+	SELECT trajs.tazid, trajs.geom
+	FROM table_area trajs, target_taz target
+	ORDER BY st_frechetdistance(target.geom, trajs.geom) ASC 
+	LIMIT {k}
+     )
 
 SELECT tazid
 FROM table_f_dist;
