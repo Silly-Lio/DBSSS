3b60edc6be1c0510759849bafdc2bf2bafe3a5be
ZherenLiu <65768893+Silly-Lio@users.noreply.github.com>
2022-10-24 12:53:50 +0800

    Update SQL_SimilarSeqSearch_StrictSimilar.sql

--- SQL_SimilarSeqSearch_StrictSimilar.sql
+++ SQL_SimilarSeqSearch_StrictSimilar.sql
@@ -1,9 +1,11 @@
-WITH target_taz AS ( -- 0. 表格生成与存储
+WITH target_taz AS ( -- 1. Get the query series
 	SELECT *
 	FROM {dataset} trajs
 	WHERE trajs.tazid = {target_tazid}
      ), 
-     cp_dist AS (
+     
+     -- Filter1: Distance between centroids 
+     cp_dist AS ( -- Filter1.1: Distance filter with spatial index. Operator "<->" and keyword "LATERAL" can do (rough) NN-query with spatial index
 	SELECT matchtrajs.tazid
 	FROM (  SELECT * 
 		FROM cp_trajs
@@ -12,18 +14,22 @@ WITH target_taz AS ( -- 0. 表格生成与存储
 		 FROM cp_trajs cp_trajs
 		 ORDER BY target.geom <-> cp_trajs.geom ASC 
 		 LIMIT {k*5}) as matchtrajs
-     ), table_cp_dist AS (
+     ), 
+     table_cp_dist AS ( -- Filter1.2: Accurate distance between centroids
 	SELECT trajs.*
 	FROM cp_dist 
 	INNER JOIN trajs trajs 
-	ON cp_dist.tazid = trajs.tazid -- ORDER BY st_centroid(target.geom) <-> st_centroid(trajs.geom) ASC 
-     ), table_area AS ( -- 2. 多边形面积
+	ON cp_dist.tazid = trajs.tazid 
+     ), 
+     
+     table_area AS ( -- Filter2: Areal difference
 	SELECT trajs.tazid, trajs.geom
 	FROM table_cp_dist trajs, target_taz target
 	ORDER BY ABS(st_area(st_envelope(target.geom)) - st_area(st_envelope(trajs.geom))) ASC 
 	LIMIT {k*2}
      ), 
-	table_f_dist AS ( -- 3. Frechet 距离
+     
+     table_f_dist AS ( -- Filter3: Frechet Distance
 	SELECT trajs.tazid, trajs.geom
 	FROM table_area trajs, target_taz target
 	ORDER BY st_frechetdistance(target.geom, trajs.geom) ASC 
